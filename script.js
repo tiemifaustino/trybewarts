8de30bcdb46f3e4e6e81dd7fbbdd7cb4219d7fc8
Tiemi Faustino <hayashipharma@gmail.com>
2022-01-11 19:09:32 -0300

    alteracoes

--- script.js
+++ script.js
@@ -12,10 +12,12 @@ function testeLogin() {
 
 btnLogin.addEventListener('click', testeLogin);
 
-/* Desabilitar o botão Source link: https://cursos.alura.com.br/forum/topico-habitar-desabilitar-botao-65202?gclid=Cj0KCQiA8vSOBhCkARIsAGdp6RTjUs8UxBi6LoAq_GBksKXT9b9KgDsQI_6GU4fbs8uL342fMSOFJIQaAnEtEALw_wcB */
+/* Desabilitar o botão. Source link: https://cursos.alura.com.br/forum/topico-habitar-desabilitar-botao-65202?gclid=Cj0KCQiA8vSOBhCkARIsAGdp6RTjUs8UxBi6LoAq_GBksKXT9b9KgDsQI_6GU4fbs8uL342fMSOFJIQaAnEtEALw_wcB */
 const buttonSubmit = document.getElementById('submit-btn');
 const checkAgreement = document.getElementById('agreement');
 
+
+/* Verificar se o checkbox está marcado. Source link: https://www.horadecodar.com.br/2020/07/28/como-verificar-se-um-checkbox-esta-checado-c-javascript-ou-jquery/ */
 buttonSubmit.disabled = true;
 
 function habilitaBotao() {
